581f2d7b2cdadb4b1dac9671eaec6d3eb888307f
Ali Shalabi <ashalabi13@gmail.com>
2019-11-30 16:45:23 -0800

    Working on combinations file

---
+++ get_combinations.py
@@ -0,0 +1,10 @@
+sample_board = [['C', 'J', 'Z', 'E'], ['V', 'A', 'X', 'B'],
+                ['X', 'N', 'T', 'U'], ['I', 'A', 'N', 'K']]
+
+sample_dict = ["CAT", "DOG", "BYTE", "TUBE", "CAN", "ANT",
+               "CAR", "TANK"]
+
+
+def get_words(length=3, board=sample_board, dict=sample_dict):
+    for i in sample_board[0]:
+        for j in sample
